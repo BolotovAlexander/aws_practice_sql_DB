0e2e6e9e785115190ae683e721411ced80c6e673
Oleksandr Bolotov <oleksandr_bolotov@epam.com>
2023-11-19 16:54:09 +0200

    update

--- workWithSQLTables/fill_tables.sql
+++ workWithSQLTables/fill_tables.sql
@@ -1,40 +1,14 @@
---First cart
 WITH inserted_cart_id AS (
   INSERT INTO carts (user_id, created_at, updated_at, status)
   VALUES
-    ('223e4567-e89b-12d3-a456-426614174001', '2023-11-11T12:00:00Z', '2023-11-11T12:30:00Z', 'OPEN')
+    ('T2xla3NhbmRyX0JvbG90b3Y6VEVTVF9QQVNTV09SRA==', '2023-11-20T12:00:00Z', '2023-11-30T12:30:00Z', 'OPEN')
   RETURNING id
 )
+// TODO change id
 
 INSERT INTO cart_items (cart_id, product_id, count)
-SELECT id, '223e4567-e89b-12d3-a456-426614174002', 2 FROM inserted_cart_id
+SELECT id, '80091538-6e85-45ca-a21f-6c7c780c8a31', 1 FROM inserted_cart_id
 UNION ALL
-SELECT id, '323e4567-e89b-12d3-a456-426614174003', 1 FROM inserted_cart_id;
-
---Second cart
-WITH inserted_cart_id AS (
-  INSERT INTO carts (user_id, created_at, updated_at, status)
-  VALUES
-    ('223e4567-e89b-12d3-a456-426614174001', '2023-11-20T12:00:00Z', '2023-11-30T12:30:00Z', 'OPEN')
-  RETURNING id
-)
-
-INSERT INTO cart_items (cart_id, product_id, count)
-SELECT id, '223e4567-e89b-12d3-a456-426614174004', 1 FROM inserted_cart_id
+SELECT id, 'a8b0ab34-2af1-4d77-b546-579b09d3ae5f', 2 FROM inserted_cart_id
 UNION ALL
-SELECT id, '323e4567-e89b-12d3-a456-426614174005', 10 FROM inserted_cart_id
-UNION ALL
-SELECT id, '323e4567-e89b-12d3-a456-426614174006', 3 FROM inserted_cart_id;
-
---Third cart
-WITH inserted_cart_id AS (
-  INSERT INTO carts (user_id, created_at, updated_at, status)
-  VALUES
-    ('223e4567-e89b-12d3-a456-426614174001', '2023-11-02T12:00:00Z', '2023-11-30T12:30:00Z', 'OPEN')
-  RETURNING id
-)
-
-INSERT INTO cart_items (cart_id, product_id, count)
-SELECT id, '223e4567-e89b-12d3-a456-426614174007', 2 FROM inserted_cart_id
-UNION ALL
-SELECT id, '323e4567-e89b-12d3-a456-426614174008', 2 FROM inserted_cart_id;
+SELECT id, '6f3196f9-0528-43a0-8177-b54988a2b338', 3 FROM inserted_cart_id;
